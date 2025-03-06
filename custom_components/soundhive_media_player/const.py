f8f247186f5ecca5d50064f91e3c0f05c996cc2e
simonsays-techtalk <150742975+simonsays-techtalk@users.noreply.github.com>
2025-03-06 18:07:07 +0100

    Update const.py

--- custom_components/soundhive_media_player/const.py
+++ custom_components/soundhive_media_player/const.py
@@ -1,4 +1,3 @@
-# Soundhive: Constants for the Media Player Component
-# VVERSION = "1.1.02" (Initial Constants File)
+# VVERSION = "1.1.05" (Initial Constants File)
 
 DOMAIN = "soundhive_media_player"
